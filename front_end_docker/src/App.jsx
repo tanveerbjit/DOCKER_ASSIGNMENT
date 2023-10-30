14a7cf78ba222fcc59f0aeca29773e8bb547d247
Md. Tanveer Ahmed <141206427+tanveerbjit@users.noreply.github.com>
2023-10-30 12:23:03 +0600

    Update App.jsx
    update

--- front_end_docker/src/App.jsx
+++ front_end_docker/src/App.jsx
@@ -88,7 +88,7 @@ function App() {
 
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<RegistrationPage />} />
+          <Route path="/" element={<HomePage  />} />
           <Route path="/product/detail/:id" element={<DetailPage />} />
 
           <Route element={<UnAuthenticate />}>
